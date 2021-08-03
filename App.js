67205aef27a642098a535d62f9d41a53d982f85d
vanmercado <vanardenmercado@gmail.com>
2021-08-03 23:09:40 +0800

    feat: my portfolio

--- App.js
+++ App.js
@@ -5,20 +5,15 @@ const appearOptions = {
 	rootMargin: '0px 0px -40px 0px',
 };
 
-const appearOnScroll = new IntersectionObserver(function (
-	entries,
-	appearOnScroll
-) {
+const appearOnScroll = new IntersectionObserver(function (entries) {
 	entries.forEach((entry) => {
 		if (!entry.isIntersecting) {
 			return;
 		} else {
 			entry.target.classList.add('appear');
-			appearOnScroll.inobserve(entry.target);
 		}
 	});
-},
-appearOptions);
+}, appearOptions);
 
 faders.forEach((fader) => {
 	appearOnScroll.observe(fader);
